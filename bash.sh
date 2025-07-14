906aaedd12f8fbe34dda1941daaf8ee7653f8ef9
kali <kali@kali.kali>
2025-07-15 03:19:11 +0530

    new

---
+++ bash.sh
@@ -0,0 +1,15 @@
+# create a common file that installs via npm i and runs each agent via bash script
+
+
+cd ../Ezee_munjho
+npm i
+npm run start
+
+cd ../Ezee_wandr
+npm i
+npm run start
+
+cd ../appsmith
+npm i
+npm run start
+
